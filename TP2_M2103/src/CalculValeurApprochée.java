06ff67a07c325b90469ff9fee0c5c8c9f9a722ce
BLD3286A <BLD3286A@r-info-opal-e08.iut.iut-tlse3.fr>
2020-02-05 09:02:36 +0100

    commit 4

--- TP2_M2103/src/CalculValeurApprochée.java
+++ TP2_M2103/src/CalculValeurApprochée.java
@@ -2,7 +2,15 @@
 public class CalculValeurApprochée {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long a = 2L;
+		Rationnel r = new Rationnel(a, 1);
+		Rationnel unDemi = new Rationnel (1, 2);
+		for (int i=0; i < 4; i++) {
+			Rationnel v = new Rationnel(a*r.getDenominateur(), r.getNumerateur());
+			r = unDemi.produit(r.somme(v));
+		}
+		System.out.println("Valeur approchée de racine carrée de " + a +" = " + r);
+
 
 	}
 
